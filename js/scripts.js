812fc900e157851f0272f88c8d7d2e65824b867a
Lautaro Barrera <lautarobarrera12@gmail.com>
2024-05-17 14:20:33 -0300

    arreglamos el bug de cuando había más contenido en el index

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,11 @@
 document.addEventListener('DOMContentLoaded', () => {
     const burger = document.querySelector('.burger');
     const nav = document.querySelector('.nav-links');
+    const body = document.body;
 
     burger.addEventListener('click', () => {
         nav.classList.toggle('active');
         burger.classList.toggle('toggle');
+        body.classList.toggle('nav-active');
     });
 });
